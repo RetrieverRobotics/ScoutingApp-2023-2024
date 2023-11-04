784a247be8343cad28c28285fdbe95b480b46926
SZB3748 <jakengoodyear@gmail.com>
2023-11-04 03:40:23 -0400

    Finish functional verison of match scout
    I'll make it all look pretty later.

--- templates/match_scout.html.js
+++ templates/match_scout.html.js
@@ -1,21 +1,56 @@
+let endAuto = null;
+const inputs = {};
+
+function getNow() {
+    return (new Date()).toISOString();
+}
+
 window.addEventListener("load", () => {
     const matchScoutForm = document.getElementById("matchScoutForm");
+    /** @type {HTMLButtonElement} */
+    const matchEndButton = document.getElementById("matchEndButton");
 
     matchScoutForm.addEventListener("submit", (ev) => {
         ev.preventDefault();
 
-        if (!matchScoutForm.reportValidity())
-            return;
+        const data = new FormData();
 
-        const data = new FormData(matchScoutForm);
-        //TODO unique data validation
+        inputs["end-auto"] = endAuto;
+        inputs["end"] = getNow();
 
-        const toStore = {};
-
-        for (const key of data.keys())
-            toStore[key] = data.get(key);
-
-        localStorage.setItem("MATCH", JSON.stringify(toStore));
+        localStorage.setItem("MATCH", JSON.stringify(inputs));
         location.href = "/scout/match/postmatch";
     });
+
+    matchEndButton.addEventListener("click", (ev) => {
+        if (ev.button != 0) return;
+        else if (endAuto == null) {
+            endAuto = getNow();
+            matchEndButton.innerText = "End Match";
+            changeMatchStyle();
+            setTimeout(() => matchEndButton.type = "submit", 100);
+            
+        }
+    });
+
+    initInputs();
 });
+
+function changeMatchStyle() {
+    //TODO change element colors so that change in mode is more clear
+    const matchStealButton = document.getElementById("matchStealButton");
+    const matchStolenButton = document.getElementById("matchStolenButton");
+
+    matchStealButton.style.display = "block";
+    matchStolenButton.style.display = "block";
+}
+
+function initInputs() {
+    document.querySelectorAll(".scout-input").forEach((elm) => {
+        const key = elm.innerText.toLowerCase();
+        inputs[key] = [];
+        elm.addEventListener("click", () => {
+            inputs[key].push(getNow());
+        });
+    });
+}
